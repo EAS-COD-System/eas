809ad5a97767b5c94fe7a55b3655514c544ccf81
EAS-COD-System <nnbb2222@gmail.com>
2025-10-12 17:50:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const { v4: uuidv4 } = require('uuid');
 
-const app = express();
+const app = express(); 
 const PORT = process.env.PORT || 3000;
 
 const ROOT = __dirname;
