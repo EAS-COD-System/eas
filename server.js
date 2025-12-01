cb8ea3c75518551d844d6ddf6bc94369c48658da
EAS-COD-System <nnbb2222@gmail.com>
2025-12-01 14:09:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1973,18 +1973,6 @@ async function createStartupBackup() {
 }
 
 // ======== STATIC ROUTES ========
-app.get('/audora', (req, res) => {
-  res.sendFile(path.join(ROOT, 'public', 'audora', 'index.html'));
-});
-
-app.get('/qr', (req, res) => {
-  res.sendFile(path.join(ROOT, 'public', 'qr', 'index.html'));
-});
-
-app.get('/carperfume', (req, res) => {
-  res.sendFile(path.join(ROOT, 'public', 'carperfume', 'index.html'));
-});
-
 app.get('/product.html', (req, res) => res.sendFile(path.join(ROOT, 'product.html')));
 app.get('/', (req, res) => res.sendFile(path.join(ROOT, 'index.html')));
 
